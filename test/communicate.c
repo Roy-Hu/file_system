e1bc8ee81e0a2005c2a9956c637797c9f1768c99
Qiyuan Yang <qy28@pyrite.clear7.rice.edu>
2024-04-07 11:23:07 -0500

    update test

---
+++ test/communicate.c
@@ -0,0 +1,14 @@
+#include <comp421/yalnix.h>
+#include <comp421/iolib.h>
+#include "cache.h"
+#include "yfs.h"
+int
+main()
+{
+    TracePrintf(1, "Communicate test in progress\n");
+	Create("/ssss/ssss");
+    //Create("/ssss");
+    //Create("/");
+    
+    return (0);
+}
